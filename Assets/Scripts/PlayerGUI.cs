e709d452efd3d106bd764e0021c16904ea8e7dc7
Ahmad Mahmoud Mohammad <thediamondknight1@gmail.com>
2015-08-26 01:48:32 +0200

    added survival gui script

---
+++ Assets/Scripts/PlayerGUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerGUI : MonoBehaviour {
+
+  public Vector2 TextureSize = new Vector2(240, 40);
+  //Health Variables
+  [SerializeField]
+  PlayerGUIComponent HealthComponent;
+  [SerializeField]
+  PlayerGUIComponent HungerComponent;
+  [SerializeField]
+  PlayerGUIComponent ThirstComponent;
+  [SerializeField]
+  PlayerGUIComponent StaminaComponent;
+  private CharacterMotor CharacterMotor;
+  private FirstPersonController FirstPersonController;
+  private CharacterController CharacterController;
+
+  void Start()
+  {
+    HealthComponent = new PlayerGUIComponent(TextureSize, new Vector2(20, 20), 150);
+    HungerComponent = new PlayerGUIComponent(TextureSize, new Vector2(20, 60), 150);
+    ThirstComponent = new PlayerGUIComponent(TextureSize, new Vector2(20, 100), 100);
+    StaminaComponent = new PlayerGUIComponent(TextureSize, new Vector2(20, 140), 35);
+
+    CharacterController = GetComponent<CharacterController>();
+    CharacterMotor = GetComponent<CharacterMotor>();
+    FirstPersonController = GetComponent<FirstPersonController>();
+  }
+
+  void OnGUI()
+  {
+    HealthComponent.Render();
+    HungerComponent.Render();
+    ThirstComponent.Render();
+    StaminaComponent.Render();
+  }
+}
---
+++ Assets/Scripts/PlayerGUIComponent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+  [Serializable]
+  public class PlayerGUIComponent
+  {
+    public Vector2 ComponentSize;
+    public Vector2 ComponentPosition;
+    float MaxComponentBarDisplay;
+    float ComponentBarDisplay;
+    float MinComponentBarDisplay;
+    [SerializeField]
+    private Texture2D ComponentBarFullTexture;
+    [SerializeField]
+    private Texture2D ComponentBarEmptyTexture;
+    [SerializeField]
+    private int FallRate;
+    [SerializeField]
+    //public float FallRateMultiplier;
+    public PlayerGUIComponent(Vector2 componentSize, Vector2 componentPosition, int fallRate) : this(componentSize, componentPosition, fallRate, null, null)
+    {
+      
+    }
+    public PlayerGUIComponent(Vector2 componentSize, Vector2 componentPosition, int fallRate, Texture2D componentBarFullTexture, Texture2D componentBarEmptyTexture, float maxComponentBarDisplay = 1, float minComponentBarDisplay = 0)
+    {
+      this.ComponentPosition = componentPosition;
+      this.MaxComponentBarDisplay = maxComponentBarDisplay;
+      this.MinComponentBarDisplay = minComponentBarDisplay;
+      this.ComponentBarFullTexture = componentBarFullTexture;
+      this.ComponentBarEmptyTexture = componentBarEmptyTexture;
+      this.FallRate = fallRate;
+      this.ComponentSize = componentSize;
+    }
+
+    public void Render()
+    {
+      GUI.BeginGroup(new Rect(this.ComponentPosition, this.ComponentSize));
+      GUI.Box(new Rect(Vector2.zero, this.ComponentSize), this.ComponentBarEmptyTexture);
+      
+      GUI.BeginGroup(new Rect(this.ComponentPosition, this.ComponentSize));
+      GUI.Box(new Rect(Vector2.zero.x, Vector2.zero.y, this.ComponentSize.x * this.ComponentBarDisplay, this.ComponentSize.y), this.ComponentBarFullTexture);
+      GUI.EndGroup();
+      GUI.EndGroup();
+    }
+    public bool IsEmpty()
+    {
+      return ComponentBarDisplay == 0;
+    }
+    public bool IsFull()
+    {
+      return ComponentBarDisplay == 1;
+    }
+    public void Update(int multiplier = 1)
+    {
+      if (ComponentBarDisplay > 0) 
+      {
+        ComponentBarDisplay -= Time.deltaTime / FallRate * multiplier;
+        if (ComponentBarDisplay < 0)
+        {
+          ComponentBarDisplay = 0;
+        }
+      }
+    }
+  }
+}
